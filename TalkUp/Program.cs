61d3043aeb1b335b44c0dd02c4f87ff4f7f1aaca
sandra-ehab <sandraehab2011@gmail.com>
2023-05-30 19:43:51 +0300

    1

--- TalkUp/Program.cs
+++ TalkUp/Program.cs
@@ -3,15 +3,19 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using TalkUp;
 using TalkUp.Controllers;
+using TalkUp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-//builder.Services.AddDbContext<ApplicationDbContext_Old>(options => options.UseSqlServer(connectionString));
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<TalkUpContext>(options => options.UseSqlServer(connectionString));
 
+builder.Services.AddDbContext<TalkUp.Models.TalkUpContext>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<TalkUp.Models.TalkUpContext>(options =>
+    options.UseSqlServer("Server = DESKTOP - 62B5GTO; Database: TalkUp; Trusted_Connection = True"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors();
